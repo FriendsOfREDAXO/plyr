92e720eb505b7fcaf2cd69965055303a2157d9e8
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-28 00:17:56 +0200

    license info

--- boot.php
+++ boot.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the videopackage.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 $this->setProperty('author', 'Friends Of REDAXO');
 
