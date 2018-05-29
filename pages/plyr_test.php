557dede75b90b7c5a25f7afdc90dcb6b59e8cd47
Thomas Skerbis <thomas.skerbis@mac.com>
2018-05-29 13:47:34 +0200

    Update plyr_test.php

--- pages/plyr_test.php
+++ pages/plyr_test.php
@@ -6,7 +6,7 @@ $hideControls = rex_config::get('video', 'controls_plyr');
 $clickToPlay = rex_config::get('video', 'click_plyr');
 //YOUTUBE
 	echo '<h2>Plyr Video</h2>';
-	echo '<div class="rex_video '.$autoplayStandard.' '.$hideControls.' '.$clickToPlay.'" data-type="youtube"  data-video-id="YH3c1QZzRK4"></div></br>';
+	echo '<div class="rex_video '.$autoplayStandard.' '.$hideControls.' '.$clickToPlay.'" data-type="youtube"  data-video-id="TT1aHnfujcs"></div></br>';
 	echo '<div style="text-align: center;"><a href="https://www.youtube-nocookie.com/watch?v=TT1aHnfujcs"><i class="fa fa-youtube" aria-hidden="true"></i>Video-Credits</a></div>';
 	
 //Variablen AUDIO
