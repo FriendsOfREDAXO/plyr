de1f5ea61b4a43b0bbcc78fb90326998e4c20aad
Thomas Skerbis <thomas.skerbis@mac.com>
2018-05-29 13:45:23 +0200

    Update afterglow_test.php

--- pages/afterglow_test.php
+++ pages/afterglow_test.php
@@ -6,5 +6,5 @@ $sounds = rex_config::get('video', 'sound_afterglow');
 $theme = rex_config::get('video', 'theme_afterglow');
 // YOUTUBE
 	echo '<video autoplay="'.$autoplay.'" data-volume="'.$sounds.'" data-skin="'.$theme.'" id="video1" class="afterglow" width="1920" height="1080" data-youtube-id="YH3c1QZzRK4" data-autoresize="fit"></video></br>';
-		echo '<div style="text-align: center;"><a href="https://www.youtube.com/watch?v=YH3c1QZzRK4"><i class="fa fa-youtube" aria-hidden="true"></i>Video-Credits</a></div>';
+		echo '<div style="text-align: center;"><a href="https://www.youtube-nocookie.com/watch?v=TT1aHnfujcs"><i class="fa fa-youtube" aria-hidden="true"></i>Video-Credits</a></div>';
 ?>
