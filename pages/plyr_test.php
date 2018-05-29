bd21fc06d995316fd6193236fe46f28417a3a24f
Thomas Skerbis <thomas.skerbis@mac.com>
2018-05-29 13:44:52 +0200

    Wolfgangs Video integriert
    :-) https://www.youtube-nocookie.com/watch?v=TT1aHnfujcs

--- pages/plyr_test.php
+++ pages/plyr_test.php
@@ -7,7 +7,7 @@ $clickToPlay = rex_config::get('video', 'click_plyr');
 //YOUTUBE
 	echo '<h2>Plyr Video</h2>';
 	echo '<div class="rex_video '.$autoplayStandard.' '.$hideControls.' '.$clickToPlay.'" data-type="youtube"  data-video-id="YH3c1QZzRK4"></div></br>';
-	echo '<div style="text-align: center;"><a href="https://www.youtube.com/watch?v=YH3c1QZzRK4"><i class="fa fa-youtube" aria-hidden="true"></i>Video-Credits</a></div>';
+	echo '<div style="text-align: center;"><a href="https://www.youtube-nocookie.com/watch?v=TT1aHnfujcs"><i class="fa fa-youtube" aria-hidden="true"></i>Video-Credits</a></div>';
 	
 //Variablen AUDIO
 
