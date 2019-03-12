832284720410b2046509c8123307ffd8c9f5624b
Thomas Skerbis <skerbis@klxm.de>
2019-03-12 23:18:35 +0100

    Delete help.php

--- pages/help.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = rex_file::get(rex_path::addon('video','README.md'));
-$body = rex_markdown::factory()->parse($file);
-$fragment = new rex_fragment();
-$fragment->setVar('body', $body, false);
-$content = $fragment->parse('core/page/section.php');
-echo $content;
-?>
