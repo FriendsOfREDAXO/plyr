2b9cf106c911c15061206bfd41113111b9255ae6
Thomas Skerbis <skerbis@klxm.de>
2019-08-12 17:16:20 +0200

    Erste Aufräumarbeiten

--- boot.php
+++ boot.php
@@ -12,40 +12,5 @@
 $this->setProperty('author', 'Friends Of REDAXO');
 
 if (rex::isBackend() && is_object(rex::getUser())) {
-    rex_perm::register('video');
-    rex_perm::register('video[config]');
-
-    /////////////////////////
-	//// init Variablen  ////
-	/////////////////////////
-	if($this->getConfig('player') == 'Plyr') {
-		$page = $this->getProperty('page');
-    	$page['subpages']['plyr'] = ['title' =>  $this->i18n('plyr_title')];
-    	$page['subpages']['plyr_test'] = ['title' => $this->i18n('plyr_test')];
-    	$this->setProperty('page', $page);
-	}
-
-	if($this->getConfig('player') == 'Afterglow') {
-		$page = $this->getProperty('page');
-    	$page['subpages']['afterglow'] = ['title' =>  $this->i18n('afterglow_title')];
-    	$page['subpages']['afterglow_test'] = ['title' => $this->i18n('afterglow_test')];
-    	$this->setProperty('page', $page);
-	}
-	if($this->getConfig('player') == 'Audio') {
-		$page = $this->getProperty('page');
-    	$page['subpages']['audio'] = ['title' =>  $this->i18n('audio_title')];
-    	$page['subpages']['audio_test'] = ['title' => $this->i18n('audio_test')];
-    	$this->setProperty('page', $page);
-	}
-	
-	if($this->getConfig('player') == 'Plyr') {
-    	rex_view::addCssFile($this->getAssetsUrl('Plyr/plyr.css'));
-    	rex_view::addJsFile($this->getAssetsUrl('Plyr/plyr.min.js'));   
-    	rex_view::addJsFile($this->getAssetsUrl('js/plyr_video_backend.js'));
-	}
-
-	if($this->getConfig('player') == 'Afterglow') {
-		rex_view::addJsFile($this->getAssetsUrl('Afterglow/afterglow.min.js'));
-		rex_view::addJsFile($this->getAssetsUrl('js/afterglow.js'));
-	}
+   
 }
--- lib/video_functions.php
+++ lib/video_functions.php
@@ -104,8 +104,7 @@ public static function outputVideo($url,$poster=NULL)
         $player = new rex_video();
         $link = $player->getVideoType($url);
         // GET STANDARD CONFIG VARIABLES
-        if (rex_config::get('video', 'player') == 'Plyr')
-        {
+
             $autoplayStandard = rex_config::get('video', 'autoplay_plyr');
             $hideControls = rex_config::get('video', 'controls_plyr');
             $clickToPlay = rex_config::get('video', 'click_plyr');
@@ -114,26 +113,22 @@ public static function outputVideo($url,$poster=NULL)
 
             if ($player->checkYoutube($link) == true)
             {
-                $out = '<div class="rex_video ' . $autoplayStandard . ' ' . $hideControls . ' ' . $clickToPlay . '" data-plyr-provider="youtube" data-plyr-embed-id="' . $player->getYoutubeId($link) . '"></div></br>';
+                $out = '<div class="rex_video" data-plyr-provider="youtube" data-plyr-embed-id="' . $player->getYoutubeId($link) . '"></div></br>';
             }
             if ($player->checkVimeo($link) == true)
             {
-                $out = '<div class="rex_video ' . $hideControls . ' ' . $autoplayStandard . ' ' . $clickToPlay . '" data-plyr-provider="vimeo" data-plyr-embed-id="' . $player->getVimeoId($link) . '"></div></br>';
+                $out = '<div class="rex_video" data-plyr-provider="vimeo" data-plyr-embed-id="' . $player->getVimeoId($link) . '"></div></br>';
             }
             if ($player->checkMedia($url) !== false)
             {
-                if ($autoplayStandard == 'Ja')
-                {
-                    $localAutoplay = "autoplay";
-                }
 
                 if ($poster)
                     {
                     $poster = ' poster="'.$poster.'"';
-                }
+                    }
 
                 $out = '
-						<video class="rex_video ' . $hideControls . ' ' . $autoplayStandard . ' ' . $clickToPlay . '" ' . $localAutoplay . ' playsinline volume=1'.$poster.'>
+						<video class="rex_video" ' . $localAutoplay . ' playsinline volume=1'.$poster.'>
 							<source src="' . $link . '" type="video/mp4">
 						</video>
 					';
@@ -148,36 +143,7 @@ public static function outputVideo($url,$poster=NULL)
 						</audio>
 					';
             }
-        }
-        if (rex_config::get('video', 'player') == 'Afterglow')
-        {
-            $autoplay = rex_config::get('video', 'autoplay_afterglow');
-            $sounds = rex_config::get('video', 'sound_afterglow');
-            $theme = rex_config::get('video', 'theme_afterglow');
-
-            if ($player->checkYoutube($link) == true)
-            {
-                $out = '
-			        <video  autoplay="' . $autoplay . '" data-volume="' . $sounds . '" data-skin="' . $theme . '" class="afterglow" id="video1" width="1920" height="1080"  data-youtube-id="' . $player->getYoutubeId($link) . '" data-autoresize="fit"></video>
-			        ';
-            }
-
-            if ($player->checkVimeo($link) == true)
-            {
-                $out = '
-					 <video class="afterglow" id="myvideo" width="960" height="540" data-vimeo-id="' . $player->getVimeoId($link) . '"></video>
-					';
-            }
-
-            if ($player->checkMedia($url) !== false)
-            {
-                $out = '
-			        <video autoplay="' . $autoplay . '" data-volume="' . $sounds . '" data-skin="' . $theme . '" class="afterglow" id="myvideo" width="1080" height="720">
-			            <source type="video/mp4" src="' . $link . '" />
-			        </video>
-			        ';
-            }
-        }
+        
         return $out;
     }
 
