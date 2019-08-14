94141166ed38a204f70991eb3b8072202437287c
Thomas Skerbis <thomas.skerbis@mac.com>
2019-08-14 13:36:24 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -8,9 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+$plyr = rex_addon::get('matomo');
 if (rex::isBackend() && is_object(rex::getUser())) {
-      rex_view::addCssFile($this->getAssetsUrl('vendor/plyr/dist/plyr.css'));
-    	rex_view::addJsFile($this->getAssetsUrl('vendor/plyr/dist/plyr.min.js'));   
-    	rex_view::addJsFile($this->getAssetsUrl('plyr_be_init.js'));
+    rex_view::addCssFile($plyr->getAssetsUrl('vendor/plyr/dist/plyr.css'));
+    rex_view::addJsFile($plyr->getAssetsUrl('vendor/plyr/dist/plyr.min.js'));   
+    rex_view::addJsFile($plyr->getAssetsUrl('plyr_be_init.js'));
 }
