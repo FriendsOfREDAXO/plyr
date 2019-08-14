7a2d1ddbdde6472a35479f2f1639d91b99425fb1
Thomas Skerbis <thomas.skerbis@mac.com>
2019-08-14 21:29:35 +0200

    Update rex_var_plyr.php

--- lib/rex_var_plyr.php
+++ lib/rex_var_plyr.php
@@ -13,7 +13,7 @@ class rex_var_plyr extends rex_var
 {
    protected function getOutput() 
    {
-
+   $controls=$poster=NULL;
     $id = $this->getArg('id', 0, true);
         if (!in_array($this->getContext(), ['module', 'action']) || !is_numeric($id) || $id < 1 || $id > 20) {
             return false;
