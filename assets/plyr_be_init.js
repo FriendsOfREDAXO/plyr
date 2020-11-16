b5534a7312601e88641b55d8ee550abf3acc957e
Thomas Skerbis <thomas.skerbis@mac.com>
2020-11-16 15:31:17 +0100

    Update plyr_be_init.js

--- assets/plyr_be_init.js
+++ assets/plyr_be_init.js
@@ -1,5 +1,11 @@
 $(document).on('rex:ready',function() {
 
-const players = Plyr.setup('.rex-plyr');
+ const players = Plyr.setup('.rex-plyr',{
+	 youtube: { 
+		 noCookie: true
+	 },
+	 iconUrl: 'assets/addons/plyr/vendor/plyr/dist/plyr.svg',
+   blankVideo: 'assets/addons/plyr/vendor/plyr/dist/blank.mp4'
+ });
 
 });
