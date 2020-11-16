1ee30a782dcb1348e02daee4b8b68f45ffe27cd5
Thomas Skerbis <thomas.skerbis@mac.com>
2020-11-16 15:29:53 +0100

    sag sprite & blank.mp4 local

--- assets/plyr_init.js
+++ assets/plyr_init.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
  const players = Plyr.setup('.rex-plyr',{
 	 youtube: { 
 		 noCookie: true
-	 }
+	 },
+	 iconUrl: 'assets/addons/plyr/vendor/plyr/dist/plyr.svg',
+         blankVideo: 'assets/addons/plyr/vendor/plyr/dist/blank.mp4'
  });
 });
