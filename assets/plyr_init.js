e687ad8d37ee8bcacb7f38d8fb65dd0d0dfb321e
Thomas Skerbis <thomas.skerbis@mac.com>
2020-12-11 12:21:31 +0100

    Update plyr_init.js

--- assets/plyr_init.js
+++ assets/plyr_init.js
@@ -8,17 +8,5 @@ document.addEventListener("DOMContentLoaded", function(){
 	 },
 	 iconUrl: '/assets/addons/plyr/vendor/plyr/dist/plyr.svg',
          blankVideo: '/assets/addons/plyr/vendor/plyr/dist/blank.mp4'
- });
-	
- const players = Plyr.setup('.rex-plyr-consent',{
-	 youtube: { 
-		 noCookie: true
-	 },
-	 vimeo: {
-	        dnt: false
-	 },
-	 iconUrl: '/assets/addons/plyr/vendor/plyr/dist/plyr.svg',
-         blankVideo: '/assets/addons/plyr/vendor/plyr/dist/blank.mp4'
  });	
-	
 });
