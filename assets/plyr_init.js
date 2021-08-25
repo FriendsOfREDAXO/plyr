a4472f6e03aa6fa86061e8ca2e308f0b9f6ec21b
Thomas Skerbis <thomas.skerbis@mac.com>
2021-08-25 13:48:43 +0200

    Update plyr_init.js

--- assets/plyr_init.js
+++ assets/plyr_init.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
- var players = Plyr.setup('.rex-plyr',{
-	 youtube: { 
-		 noCookie: true
-	 },
-	 vimeo: {
-	        dnt: false
-	 },
-	 iconUrl: '/assets/addons/plyr/vendor/plyr/dist/plyr.svg',
-         blankVideo: '/assets/addons/plyr/vendor/plyr/dist/blank.mp4'
- });	
- // Stop other videos    
- players.forEach(function(player) {
-  player.on('play',function(){
-    var others = players.filter(other => other != player)
-    others.forEach(function(other) {
-      other.pause();
-    })
-  });
-});   
-
+document.addEventListener("DOMContentLoaded", function () {
+    var players = Plyr.setup('.rex-plyr', {
+        youtube: {
+            noCookie: true
+        },
+        vimeo: {
+            dnt: false
+        },
+        iconUrl: '/assets/addons/plyr/vendor/plyr/dist/plyr.svg',
+        blankVideo: '/assets/addons/plyr/vendor/plyr/dist/blank.mp4'
+    });
+    // Stop other videos    
+    players.forEach(function (player) {
+        player.on('play', function () {
+            var others = players.filter(other => other != player)
+            others.forEach(function (other) {
+                other.pause();
+            })
+        });
+    });
 });
 
