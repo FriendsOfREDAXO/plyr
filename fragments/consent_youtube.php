ff9421a8efd144f7eeac4e6cc5a6ed1de3f541cc
Thomas Skerbis <skerbis@klxm.de>
2022-08-04 15:44:22 +0200

    YouTube-Fragment

--- fragments/consent_youtube.php
+++ fragments/consent_youtube.php
@@ -1,1 +1,8 @@
-
+<div class="aspect-ratio-16-9 plyr-consent-box">
+        <div class="plyr-consent-box-content">
+            <h2>Externes Video: Youtube</h2>
+            <p>Bitte aktivieren Sie die Optionen zur Darstellung der Vimeo-Videos in den Datenschutzeinstellungen</p>
+            <p><a class="plyr-consent-box-content-button btn btn-primary uk-button-primary consent_manager-show-box-reload">Cookie Einstellungen bearbeiten</a></p>
+            <p><a title="Video aufrufen beim Anbieter" href="<?=$this->prepend?>">Video auf youtube.com betrachten</a></p>
+        </div>
+</div>
