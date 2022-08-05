334656f82bf45904cb2c0b486e3cf5dd9995a435
Thomas Skerbis <skerbis@klxm.de>
2022-08-05 12:09:23 +0200

    Delete consent_vimeo.php

--- fragments/consent_vimeo.php
+++
@@ -1,8 +0,0 @@
-<div class="aspect-ratio-16-9 plyr-consent-box">
-        <div class="plyr-consent-box-content">
-            <h2>Externes Video: Vimeo</h2>
-            <p>Bitte aktivieren Sie die Optionen zur Darstellung der Vimeo-Videos in den Datenschutzeinstellungen</p>
-            <p><a class="btn btn-primary uk-button-primary consent_manager-show-box-reload">Cookie Einstellungen bearbeiten</a></p>
-            <p><a title="Video aufrufen beim Anbieter" href="<?=$this->prepend?>">Video auf vimeo.com betrachten</a></p>
-        </div>
-</div>
