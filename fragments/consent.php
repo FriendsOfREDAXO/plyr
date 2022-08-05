192abbb05cd6a0eefe4a74566c0f62dabcf4ac88
Thomas Skerbis <skerbis@klxm.de>
2022-08-05 12:18:17 +0200

    Update and rename consent_youtube.php to consent.php

---
+++ fragments/consent.php
@@ -0,0 +1,20 @@
+<?php if($this->type == youtube ):?>
+<div class="aspect-ratio-16-9 plyr-consent-box">
+        <div class="plyr-consent-box-content">
+            <h2>Externes Video: Youtube</h2>
+            <p class="plyr-consent-text">Bitte aktivieren Sie die Optionen zur Darstellung der Youtube-Videos in den Datenschutzeinstellungen</p>
+            <p><a class="plyr-consent-box-content-button btn btn-primary uk-button-primary consent_manager-show-box-reload">Cookie Einstellungen bearbeiten</a></p>
+            <p><a class="plyr-source-link" title="Video aufrufen beim Anbieter" href="<?=$this->url?>">Video auf youtube.com betrachten</a></p>
+        </div>
+</div>
+<?php endif; ?>
+<?php if($this->type == vimeo ):?>
+<div class="aspect-ratio-16-9 plyr-consent-box">
+        <div class="plyr-consent-box-content">
+            <h2>Externes Video: Vimeo</h2>
+            <p class="plyr-consent-text">Bitte aktivieren Sie die Optionen zur Darstellung der Vimeo-Videos in den Datenschutzeinstellungen</p>
+            <p><a class="plyr-consent-box-content-button btn btn-primary uk-button-primary consent_manager-show-box-reload">Cookie Einstellungen bearbeiten</a></p>
+            <p><a class="plyr-source-link" title="Video aufrufen beim Anbieter" href="<?=$this->url?>">Video auf vimeo.com betrachten</a></p>
+        </div>
+</div>
+<?php endif; ?>
